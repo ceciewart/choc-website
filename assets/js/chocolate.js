20f0d2b45d79c9d3a06b89294b30e62541614605
Cecilia Ewart <ceciliaewart@gmail.com>
2023-01-08 14:40:25 +0000

    first commit

---
+++ assets/js/chocolate.js
@@ -0,0 +1,24 @@
+console.log("Check it's working");
+
+// Products Panel
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $("#mainNavbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
